f1e18d92bedb3f0478f2a0f0acb84b823bdcc747
Theodliu <theodtheodliu@gmail.com>
2022-06-17 20:47:22 -0400

    Update addactor/api
    update addactor/api in ReqHandler.java

--- src/main/java/ca/utoronto/utm/mcs/ReqHandler.java
+++ src/main/java/ca/utoronto/utm/mcs/ReqHandler.java
@@ -1,9 +1,12 @@
 package ca.utoronto.utm.mcs;
 
 import java.io.IOException;
+import java.net.URI;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import javax.inject.Inject;
 
@@ -18,7 +21,7 @@ public class ReqHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        /*
+
         try {
             switch (exchange.getRequestMethod()) {
                 case "GET":
@@ -33,15 +36,45 @@ public class ReqHandler implements HttpHandler {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        */
+
     }
-    /*
+
     public void handleGet(HttpExchange exchange) throws IOException, JSONException {
+        String body = Utils.convert(exchange.getRequestBody());
+        String path = exchange.getRequestURI().getPath();
         try {
-            switch (exchange.get)
+            JSONObject deserialized = new JSONObject(body);
+
+            String name, movieId, actorId,
+            switch(path){
+                //distinguish the path
+                case "/api/v1/addActor":
+                    if(deserialized.has("name") && deserialized.has("actorId")){
+                        name = deserialized.getString("name");
+                        actorId = deserialized.getString("actorId");
+                    } else{
+                        exchange.sendResponseHeaders(400, -1);
+                        return;
+                    }
+                    try{
+                        this.dao.insertActor(name, actorId);
+                        break;
+                    } catch (Exception e){
+                        exchange.sendResponseHeaders(500, -1);
+                        e.printStackTrace();
+                        return;
+                    }
+                    exchange.sendResponseHeaders(200, -1);
+                    break;
+
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+            exchange.sendResponseHeaders(500, -1);
         }
     }
 
-    */
+
 
 }
